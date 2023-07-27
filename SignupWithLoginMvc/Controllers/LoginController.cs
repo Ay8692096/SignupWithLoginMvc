33cfd497505d21782df9c7074ab4413c26565b20
15DA0435TX (2YV8) <15DA0435TX (2YV8)@LAPTOP-7T0NOLAV>
2023-07-27 15:16:20 +0530

    Add project files.

---
+++ SignupWithLoginMvc/Controllers/LoginController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SignupWithLoginMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.ModelBinding;
+using System.Web.Mvc;
+using System.Web.Mvc;
+//using ActionResult = Microsoft.AspNetCore.Mvc.ActionResult;
+//using ActionResult = System.Web.Mvc.ActionResult;
+//using ActionResult Signup()
+
+namespace SignupWithLoginMvc.Controllers
+{
+    public class LoginController : Controller
+    {
+        SignupLoginEntities db = new SignupLoginEntities();
+        // GET: Login
+        public System.Web.Mvc.ActionResult Index()
+        {
+            return View();
+        }
+        [System.Web.Mvc.HttpPost]
+        public System.Web.Mvc.ActionResult Index(user u)
+        {
+            var user = db.users.Where(model => model.username == u.username && model.password == u.password).FirstOrDefault();
+            if(user!=null)
+            {
+                Session["UserId"]=u.Id.ToString();
+                Session["Username"]=u.username.ToString();
+                TempData["LoginSuccessMessage"] = "<script>alert('Login Successfully !!')</script>";
+                return RedirectToAction("Index","User");
+
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "<script>alert('Username or Password is invalid !!')</script>";
+                return View();
+            }
+            return View();
+        }
+
+        public System.Web.Mvc.ActionResult Signup()
+        {
+            return View();
+        }
+
+        [System.Web.Mvc.HttpPost]
+        public System.Web.Mvc.ActionResult Signup(user u)
+        {
+            if(ModelState.IsValid == true)
+            {
+                db.users.Add(u);
+                int a = db.SaveChanges();
+                if(a>0)
+                {
+                    ViewBag.InsertMessage = "<script>alert('Registered Successfully !!')</script>";
+                    ModelState.Clear();
+                }
+                else
+                {
+                    ViewBag.InsertMessage = "<script>alert('Register failed !!')</script>";
+                }
+            }
+            return View();
+        }
+
+    }
+}
